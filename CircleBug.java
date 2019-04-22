600633a1ba9f9232c4fd313dec3a4450eb656075
AwpData <awpdata@gmail.com>
2019-04-22 09:41:16 -0700

    Create CircleBug.java

---
+++ CircleBug.java
@@ -0,0 +1,23 @@
+import info.gridworld.actor.Bug;
+
+public class CircleBug extends Bug {
+	private int steps;
+
+	public CircleBug() {
+		steps = 0;
+	}
+
+	public void act() {
+		if (steps < 2 && canMove()) {
+			move();
+			steps++;
+		} else {
+			turn();
+			steps = 0;
+		}
+	}
+
+	public void turn() {
+		setDirection(getDirection() + 45);
+	}
+}
