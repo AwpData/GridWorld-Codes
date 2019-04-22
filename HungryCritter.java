befb58d9432a57312dfdc2807a8c36b9f95091aa
AwpData <awpdata@gmail.com>
2019-04-22 16:46:27 -0700

    Create HungryCritter.java

---
+++ HungryCritter.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import info.gridworld.actor.Actor;
+
+public class HungryCritter extends ChameleonCritter {
+	
+    public void processActors(ArrayList<Actor> actors) {
+        int n = actors.size();
+        if (n == 0)
+            return;
+        int r = (int) (Math.random() * n);
+
+        Actor other = actors.get(r);
+        setColor(other.getColor());
+        other.removeSelfFromGrid();
+    }
+}
