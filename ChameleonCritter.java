2f18b0952314bc861f87716a7b3179a23bbcc330
AwpData <awpdata@gmail.com>
2019-04-22 16:46:13 -0700

    Create ChameleonCritter.java

---
+++ ChameleonCritter.java
@@ -0,0 +1,32 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+
+import java.util.ArrayList;
+
+
+public class ChameleonCritter extends Critter
+{
+    /**
+     * Randomly selects a neighbor and changes this critter's color to be the
+     * same as that neighbor's. If there are no neighbors, no action is taken.
+     */
+    public void processActors(ArrayList<Actor> actors)
+    {
+        int n = actors.size();
+        if (n == 0)
+            return;
+        int r = (int) (Math.random() * n);
+        Actor other = actors.get(r);
+        setColor(other.getColor());
+    }
+
+    /**
+     * Turns towards the new location as it moves.
+     */
+    public void makeMove(Location loc)
+    {
+        setDirection(getLocation().getDirectionToward(loc));
+        super.makeMove(loc);
+    }
+}
