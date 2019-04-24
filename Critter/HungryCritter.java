9950056c408f81cb84e49f0c18d703c4fd2dc69b
AwpData <awpdata@gmail.com>
2019-04-24 09:17:14 -0700

    Update HungryCritter.java

--- Critter/HungryCritter.java
+++ Critter/HungryCritter.java
@@ -1,7 +1,12 @@
+import java.awt.Color;
 import java.util.*;
 import info.gridworld.actor.Actor;
 
 public class HungryCritter extends ChameleonCritter {
+
+	public HungryCritter(Color color) {
+		this.setColor(color);
+	}
 
 	@Override
 	public void processActors(ArrayList<Actor> actors) {
