a3a8ef9429901bb6d3dc765ab4d99c933ebe640e
AwpData <awpdata@gmail.com>
2019-04-24 09:17:29 -0700

    Update RockCritter.java

--- Critter/RockCritter.java
+++ Critter/RockCritter.java
@@ -1,18 +1,22 @@
 import java.util.*;
-
+import java.awt.Color;
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.Critter;
 import info.gridworld.actor.Rock;
 import info.gridworld.grid.Location;
 
 public class RockCritter extends Critter {
-	
+
+	public RockCritter() {
+		this.setColor(Color.RED);
+	}
+
 	@Override
-	public void processActors(ArrayList<Actor> actors) { 
+	public void processActors(ArrayList<Actor> actors) {
 		for (Actor a : actors) {
 			if (!(a instanceof Rock) && !(a instanceof Critter)) {
 				Location loc = a.getLocation();
-				this.getGrid().put(loc, new Rock());
+				this.getGrid().put(loc, new Rock(a.getColor()));
 			}
 		}
 	}
