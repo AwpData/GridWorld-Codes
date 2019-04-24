f540fa2bb04a1abb239eb64d0f89a0e062e48a5c
AwpData <awpdata@gmail.com>
2019-04-24 09:16:57 -0700

    Update CritterRunner.java

--- Critter/CritterRunner.java
+++ Critter/CritterRunner.java
@@ -9,11 +9,11 @@ import java.awt.Color;
 public class CritterRunner {
 	public static void main(String[] args) {
 		ActorWorld world = new ActorWorld();
-		HungryCritter critter = new HungryCritter();
-		//RockCritter critter = new RockCritter();
-		critter.setColor(Color.BLACK);
-		world.add(new Location(7, 8), new Rock());
-		world.add(new Location(3, 3), new Rock());
+		// HungryCritter critter = new HungryCritter(Color.BLACK);
+		RockCritter critter = new RockCritter();
+		world.add(new Location(1, 2), new Rock(Color.BLUE));
+		world.add(new Location(3, 4), new Rock(Color.YELLOW));
+		world.add(new Location(5, 3), new Rock(Color.BLACK));
 		world.add(new Location(2, 8), new Flower(Color.BLUE));
 		world.add(new Location(5, 5), new Flower(Color.PINK));
 		world.add(new Location(1, 5), new Flower(Color.RED));
